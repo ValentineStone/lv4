99239aa9bc06ddc3f705bedbe08012ea8514d9d7
Иван Табакерка <40301518+IvanTabakerka@users.noreply.github.com>
2022-08-24 10:37:57 +0400

    SASS. nuxt/auth

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -35,12 +35,13 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/auth'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'https://localhost:3001'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build

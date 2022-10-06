b33f8866361cc7c8bf48f9a0b0e45b99cf6bc6f7
libre-alex <libre_maiden@protonmail.com>
2022-10-06 21:42:15 +0300

    Форма создания статьи

--- frontend/components/submission-form.vue
+++ frontend/components/submission-form.vue
@@ -2,16 +2,21 @@
     <form class="submission-form" action="#">
         <input class="title" type="text" placeholder="Введите заголовок...">
         <textarea class="text" placeholder="Введите текст статьи..."></textarea>
-        <input class="tags" type="text" placeholder="Добавьте хэштеги">
-        <input type="file" id="upload">
-        <label class="upload-button" for="upload"><img class="upload-button-img" src="upload.png" alt="Upload"></label>
+        <div class="tag-forms-add-files">
+            <input class="tags" type="text" placeholder="Добавьте хэштеги">
+            <input type="file" id="upload">
+            <label class="upload-label" for="upload"><img class="upload-button-img" src="upload.png" alt="Upload"></label>
+            <input type="file" id="upload-img">
+            <label class="upload-label-img" for="upload-img"><img class="upload-button-img" src="upload-img.png" alt="Upload"></label>
+        </div>
+        <button class="publish-btn" type="submit">Опубликовать</button>
     </form>
 </template>
 
 <style lang="css">
     .submission-form {
         width: 950px;
-        height: 770px;
+        height: 750px;
         background: #FFFFFF;
         box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.35);
         border-radius: 15px;
@@ -44,25 +49,46 @@
     }
 
     .tags {
-        width: 805px;
+        width: 750px;
         border-style: none;
         font-weight: 500;
         font-size: 24px;
-        display: block;
         align-items: center;
         line-height: 30px;
         margin-left: 30px;
-        margin-top: 15px;
+    }
+
+    .tag-forms-add-files {
+        display: grid;
+        grid-template-columns: auto 40px 70px;
+    }
+
+    .tag-forms-add-files label {
+        width: 31px;
+        height: 31px;
+        cursor: pointer;
     }
 
     #upload {
         display: none;
     }
 
-    .upload-button {
+    #upload-img {
+        display: none;
+    }
+
+    .publish-btn {
+        font-weight: 500;
+        background-color: #A8D4FC;
+        color: #F3F6FF;
+        width: 200px;
+        height: 35px;
+        box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+        border-radius: 30px;
+        border: none;
         display: block;
-        cursor: pointer;
-        margin-left: 840px;
-        margin-bottom: 20px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 100px;
     }
 </style>

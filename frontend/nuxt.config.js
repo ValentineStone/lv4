a3519ad1ca96b2cb4859d81ef16230bfb3e4eed2
Иван Табакерка <40301518+IvanTabakerka@users.noreply.github.com>
2022-11-01 23:23:24 +0400

    редактор не рендерится на стороне сервера

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -23,8 +23,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/vue-croppie.client.js',
-	'@/plugins/vue2-editor'
+    '@/plugins/vue-croppie.client.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -41,6 +40,7 @@ export default {
     '@nuxtjs/sitemap',
     '@nuxtjs/yandex-metrika',
     'bootstrap-vue/nuxt',
+    'vue2-editor/nuxt'
   ],
   auth: {
     strategies: {
--- frontend/plugins/vue2-editor.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-import { VueEditor, Quill } from "vue2-editor";
-
-Vue.use(VueEditor);
-Vue.use(Quill);

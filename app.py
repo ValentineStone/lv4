f7fec6c8d75cfa4b316d519f15759ed07579a7b1
Leonid Romanovsky <sirleonidromanovsky@gmail.com>
2023-01-04 07:00:55 +0300

    added single post page

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from os import environ
-from flask import Flask, render_template
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+from flask import Flask, render_template
+from flask import redirect, url_for
 
 
 MONGO_ADDR = environ.get('MONGO_ADDR', 'localhost')
@@ -9,7 +11,7 @@ MONGO_DBNAME = environ.get('MONGO_DBNAME', 'local')
 
 
 class Fake:
-    def find(self):
+    def find(self, *args):
         return []
 
 app = Flask(__name__)
@@ -27,10 +29,26 @@ try:
 except:
     print('No connection')
 
-@app.route("/")
-def home():
+print(posts)
+
+@app.route('/')
+def index():
+    return redirect(url_for('posts_get'))
+
+@app.route('/posts')
+def posts_get():
+    print('=========================================================================')
+    print(posts)
     return render_template(
         'pages/posts.html',
         posts=posts.find(),
+        categories=categories.find()
+    )
+
+@app.route('/post/<post_id>')
+def post_get(post_id):
+    return render_template(
+        'pages/posts.html',
+        posts=posts.find({"_id" : ObjectId(post_id)}),
         categories=categories.find()
     )
